5b0ad4d3fecf8820d9b40331354c8ba5b9316a1b
AzamatXakimov <typetiger59@gmail.com>
2022-10-19 22:10:37 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const elHeroForm = document.querySelector(".js-hero-form");
+const elName = elHeroForm.querySelector(".js-contact-name");
+const elRelationship = elHeroForm.querySelector(".js-contact-relationship");
+const elPhone = elHeroForm.querySelector(".js-contact-phone");
+const elContactList = document.querySelector(".js-contact-list");
+
+const contacts = []
+
+
+elHeroForm.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    const elNameValue = elName.value;    
+    const elRelationshipValue = elRelationship.value;
+    const elPhoneValue = elPhone.value;
+
+    let haveNumber = false;
+
+    const obj = {
+        name: elNameValue,
+        relationship: elRelationshipValue,
+        phone: elPhoneValue.replaceAll(" ", ""),
+    };
+
+    for (const num of contacts) {
+        if(num.phone == elPhoneValue){
+            haveNumber = true;
+            elPhone.classList.add("phone-ivalid");
+        }
+    }
+
+    if(!haveNumber){
+        contacts.push(obj);
+        elHeroForm.reset();
+        elContactList.innerHTML = null;
+    
+        for (const contact of contacts) {
+            const item = document.createElement("li");
+            const itembox = document.createElement("div");
+            const title = document.createElement("h3");
+            const realation = document.createElement("p");
+            const phonenumber = document.createElement("a");
+    
+            item.classList.add("col-6");
+            itembox.classList.add("rounded-3", "bg-info", "p-3");
+            title.classList.add("text-white", "mb-3",);
+            realation.classList.add("text-white", "mb-3",);
+            phonenumber.classList.add("text-white","text-decoration-none");
+    
+            title.textContent = contact.name;
+            realation.textContent = contact.relationship;
+            phonenumber.href = `tel: +${contact.phone}`;
+            phonenumber.textContent = `Phone Number: +${contact.phone}`;
+    
+            itembox.appendChild(title);
+            itembox.appendChild(realation);
+            itembox.appendChild(phonenumber);
+    
+            item.appendChild(itembox)
+    
+            elContactList.appendChild(item);
+        }
+    }
+
+})
